867ce41b6911a8b7a38cec0a1b49330f4a8d2ef2
TomasPodolak <tomas.podolak2@gmail.com>
2019-10-03 07:27:11 +0200

    Created Reactive Register Form

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule, HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { JwtModule } from '@auth0/angular-jwt';
 import { NgxGalleryModule } from 'ngx-gallery';
@@ -60,6 +60,7 @@ export class CustomHammerConfig extends HammerGestureConfig  {
       BrowserModule,
       HttpClientModule,
       FormsModule,
+      ReactiveFormsModule,
       BsDropdownModule.forRoot(),
       TabsModule.forRoot(),
       RouterModule.forRoot(appRoutes),
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
-//import { AlertifyService } from '../_services/alertify.service';
 import { Sweetalert2Service } from '../_services/sweetalert2.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -10,23 +10,31 @@ import { Sweetalert2Service } from '../_services/sweetalert2.service';
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-
   model: any = {};
+  registerForm: FormGroup;
 
-  constructor(private authService: AuthService, /*private alertify: AlertifyService,*/ private swal: Sweetalert2Service) { }
+  constructor(private authService: AuthService, private swal: Sweetalert2Service) { }
 
   ngOnInit() {
+    this.registerForm = new FormGroup({
+      username: new FormControl('', Validators.required),
+      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    }, this.passwordMatchParameter);
+  }
+
+  passwordMatchParameter(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : { 'mismatch': true };
   }
 
   register() {
-    this.authService.register(this.model).subscribe(() => {
-      //this.alertify.success('registration successfull');
+  /*  this.authService.register(this.model).subscribe(() => {
       this.swal.success('Registration successfull!');
     },
     error => {
-     // this.alertify.error(error);
      this.swal.error(error);
-    });
+    });*/
+    console.log(this.registerForm.value);
   }
 
   cancel() {
